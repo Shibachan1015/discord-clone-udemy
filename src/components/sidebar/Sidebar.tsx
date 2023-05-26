c6220e88cea8356b036d421471d4ff7cdc219ecb
Shibachan1015 <siba1015@cpost.plala.or.jp>
2023-05-26 09:33:00 +0900

    first commit

---
+++ src/components/sidebar/Sidebar.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./Sidebar.scss"
+
+const Sidebar = () => {
+    return (
+        <div className='sidebar'>
+            {/* sidebarLeft */}
+            <div className='sidebarLeft'>
+                <h1>discord</h1>
+                <h2>test</h2>
+            </div>
+            {/* sidebarLeft */}
+            <div className='sidebarRight'>
+
+            </div>
+        </div>
+    );
+};
+
+export default Sidebar;
